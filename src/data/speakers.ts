cf4dc879e7998e89cc5caf90d77b37861df19564
ronak <ronakpraja340@gmail.com>
2025-09-29 09:47:33 +0530

    added new speaker

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -1,107 +1,114 @@
 export const speakers = [
     {
-        "name": "Dr. P. K. Jain",
-        "designation": "Librarian, Institute of Economic Growth",
-        "image": "/gallery/keynote_speakers/pkjain.jpeg",
-        "id": "7",
-        "national": true
-    },
-    {
-        "name": "Dr. Debal Chandra Kar",
-        "designation": "Librarian at Dr B R Ambedkar University Delhi",
-        "image": "/gallery/keynote_speakers/debal.jpeg",
-        "id": "8",
-        "national": true
-    },
-    {
         "name": "Labibah Zain",
         "designation": "IFLA Special Interest Group : LIS in Developing Country at International Federation of Library Associations and Institutions (IFLA)",
         "image": "/gallery/keynote_speakers/labibah.jpeg",
-        "id": "9",
-        "national": true
-    },
-    {
-        "name": "Kishor Chandra Satpathy",
-        "designation": "Chief Librarian at Indian Statistical Institute, Kolkata",
-        "image": "/gallery/keynote_speakers/kishor.jpeg",
-        "id": "10",
-        "national": true
-    },
-    {
-        "name": "Dr. Praveen Babel",
-        "designation": "Research Supervisor (Library and Information Science), Banasthali University",
-        "image": "/gallery/keynote_speakers/praveen.jpeg",
-        "id": "11",
+        "id": "1",
         "national": true
     },
     {
         "name": "Nabi Hasan",
         "designation": "Head Librarian/Central Library, IIT Delhi",
         "image": "/gallery/keynote_speakers/nabi.jpeg",
-        "id": "12",
+        "id": "2",
         "national": true
     },
     {
-        "name": "Parveen Babbar",
-        "designation": "Deputy Librarian at Jawaharlal Nehru University",
-        "image": "/gallery/keynote_speakers/praveenb.jpeg",
-        "id": "13",
+        "name": "Kishor Chandra Satpathy",
+        "designation": "Chief Librarian at Indian Statistical Institute, Kolkata",
+        "image": "/gallery/keynote_speakers/kishor.jpeg",
+        "id": "3",
         "national": true
     },
     {
-        "name": "Santosh Gupta",
-        "designation": "Assistant Professor at University of Rajasthan",
-        "image": "/gallery/keynote_speakers/santosh.jpeg",
-        "id": "14",
+        "name": "Dr. P. K. Jain",
+        "designation": "Librarian, Institute of Economic Growth",
+        "image": "/gallery/keynote_speakers/pkjain.jpeg",
+        "id": "4",
         "national": true
     },
     {
-        "name": "Dr. Babita Gaur",
-        "designation": "HoD Academic Librarian, University of Delhi",
-        "image": "/gallery/keynote_speakers/babita.jpeg",
-        "id": "15",
+        "name": "Dr. Debal Chandra Kar",
+        "designation": "Librarian at Dr B R Ambedkar University Delhi",
+        "image": "/gallery/keynote_speakers/debal.jpeg",
+        "id": "5",
         "national": true
     },
     {
-        "name": "Sonal Singh",
-        "designation": "Librarian",
-        "image": "/gallery/keynote_speakers/sonal.avif",
-        "id": "16",
-        "national": true
-    },
-    {
-        "name": "Rajesh Singh",
-        "designation": "Librarian at University of Delhi",
-        "image": "/gallery/keynote_speakers/rajesh.jpeg",
-        "id": "17",
+        "name": "Dr. Neeraj Chaurasia",
+        "designation": "Deputy Librarian, Indian Institute of Technology Delhi(IIT Delhi)",
+        "image": "/gallery/keynote_speakers/neeraj.jpeg",
+        "id": "6",
         "national": true
     },
     {
         "name": "Dr. Rishi Tiwari",
         "designation": "Head & Librarian at Malaviya National Institute of Technology, Jaipur",
         "image": "/gallery/keynote_speakers/rishi.jpeg",
-        "id": "18",
+        "id": "7",
+        "national": true
+    },
+    {
+        "name": "Rajesh Singh",
+        "designation": "Librarian at University of Delhi",
+        "image": "/gallery/keynote_speakers/rajesh.jpeg",
+        "id": "8",
+        "national": true
+    },
+    {
+        "name": "Sonal Singh",
+        "designation": "Librarian",
+        "image": "/gallery/keynote_speakers/anshu.jpg",
+        "id": "9",
+        "national": true
+    },
+    {
+        "name": "Dr. Babita Gaur",
+        "designation": "HoD Academic Librarian, University of Delhi",
+        "image": "/gallery/keynote_speakers/babita.jpeg",
+        "id": "10",
+        "national": true
+    },
+    {
+        "name": "Parveen Babbar",
+        "designation": "Deputy Librarian at Jawaharlal Nehru University",
+        "image": "/gallery/keynote_speakers/praveenb.jpeg",
+        "id": "11",
+        "national": true
+    },
+    {
+        "name": "Santosh Gupta",
+        "designation": "Assistant Professor at University of Rajasthan",
+        "image": "/gallery/keynote_speakers/santosh.jpeg",
+        "id": "12",
+        "national": true
+    },
+    {
+        "name": "Dr. Praveen Babel",
+        "designation": "Research Supervisor (Library and Information Science), Banasthali University",
+        "image": "/gallery/keynote_speakers/praveen.jpeg",
+        "id": "13",
         "national": true
     },
     {
         "name": "Prof. G. A. Ghanshyam",
         "designation": "Joint Director Directorate of Higher Education, Chhattisgarh, Raipur",
         "image": "/gallery/keynote_speakers/ghanshyam.jpg",
-        "id": "19",
+        "id": "14",
         "national": true
     },
     {
         "name": "DR. Mohit Jeengar",
         "designation": "Assistant Professor in the Department of English at the University of Rajasthan, Jaipur",
         "image": "/gallery/keynote_speakers/mohit.jpg",
-        "id": "20",
+        "id": "15",
         "national": true
     },
     {
         "name": "Dr. Richa Arora",
         "designation": "Professor & Head, Department of Liberal Arts & Social Sciences, Manipal University Jaipur",
-        "image": "/gallery/keynote_speakers/richaarora.jpg",
-        "id": "21",
+        "image": "/gallery/keynote_speakers/richa.jpg",
+        "id": "16",
         "national": true
     },
 ]
